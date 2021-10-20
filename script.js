27b961878af1a74e43a310effdade245837dba91
Joel Petersson <joel.anders.petersson@gmail.com>
2021-10-20 15:42:57 +0200

    commit 7

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ window.addEventListener('resize', function(event) {
         x.style.display = "none";
     }
 }, true);
+
+function scrollToElem(id) {
+    if(document.getElementById("hamburger").style.display != 'none') {
+        document.getElementById("menuList").style.display = "none";
+    }
+    var elem = document.getElementById(id);
+    var header = document.getElementById("headerId");
+    var y = elem.getBoundingClientRect().top - document.body.getBoundingClientRect().top;
+    console.log(y-header.clientHeight);
+    window.scrollTo({top: y-header.clientHeight, behavior: 'smooth'});
+}
+
+function scrollToTop() {
+    window.scrollTo({top: 0, behavior: "smooth"});
+}
